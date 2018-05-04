73c1f6ca13b2805984b968fb5199aff672d7be83
unknown <Administrator@ITC-NB2012-14.oncbnet.go.th>
2018-05-04 11:49:36 +0700

    LINE

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from linebot.exceptions import (
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
-    ImageMessage, VideoMessage, AudioMessage
+    ImageMessage, VideoMessage, AudioMessage, StickerMessage
 )
 
 import ptt
@@ -23,9 +23,24 @@ import ptt
 app = Flask(__name__)
 
 latest_image_path = ""
+#zincBot
+#line_bot_api = LineBotApi('GnZxefH4pGG1ubbStIfsAzQ4YLvReiC3reZWrL2ttzJa1SZ3OusBPgWtoOZpzlWSLDLMNQg+W0yT1g0g+OYGNUVNi/MBUWQEcNyq76509vDXoNW8BCRPK0MJItmFosC+VfzVxZQ9cU0dVJR5W8vTOQdB04t89/1O/w1cDnyilFU=')
+#handler = WebhookHandler('b0c8111bf70a2e8063d42c49ad1dd45d')
+#BotBot
+#line_bot_api = LineBotApi('JhzjS4H2a91ZATk82inkdJp6Tpy66EB18K7AFuQD1uVzJw2/4tJnu28MS62AlJXrzKpCe/5JbgHHR5Ly4kB4dKBuMCOJTK+7/qtiZSchmTdtRhdwFxnqEUNn+sB0KG/FEhVFZqYodSIZO5zPHK+1BwdB04t89/1O/w1cDnyilFU=')
+#handler = WebhookHandler('b38646c2f6dd84fb3e2ffcf29e0f7e4b')
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 
-line_bot_api = LineBotApi('GnZxefH4pGG1ubbStIfsAzQ4YLvReiC3reZWrL2ttzJa1SZ3OusBPgWtoOZpzlWSLDLMNQg+W0yT1g0g+OYGNUVNi/MBUWQEcNyq76509vDXoNW8BCRPK0MJItmFosC+VfzVxZQ9cU0dVJR5W8vTOQdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('b0c8111bf70a2e8063d42c49ad1dd45d')
+if channel_secret is None:
+   print('Specify LINE_CHANNEL_SECRET as environment variable.')
+   sys.exit(1)
+if channel_access_token is None:
+   print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+   sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
 
 
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
@@ -43,7 +58,7 @@ def make_static_tmp_dir():
 
 @app.route("/", methods=['GET'])
 def default_action():
-    l = oil_price.get_prices()
+    l = ptt.get_prices()
     s = ""
     for p in l:
         s += "%s %f บาท\n"%(p[0],p[1])
@@ -60,16 +75,25 @@ def callback():
 
     # handle webhook body
     try:
+        print('body :' , body)
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
 
     return 'OK'
-
+@handler.add(MessageEvent, message=StickerMessage)
+def handle_sticker_message(event):
+   # Handle webhook verification
+    if event.reply_token == 'ffffffffffffffffffffffffffffffff':
+       return 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     global latest_image_path
+
+    # Handle webhook verification
+    if event.reply_token == '00000000000000000000000000000000':
+       return 
 
     if event.message.text == 'ราคาน้ำมัน':
         l = ptt.get_prices()
